608186008e2513e6fe7318d7e9a0207221a35255
Sushanth Somayaji <rksomayaji@gmail.com>
2014-09-28 08:15:32 +0530

    Merged changes from temp-merge
    Use of if-elseif-else changed to switch-case.

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -10,89 +10,98 @@ for (var i=0;i<keys.length; i++) {
         var btnVal = this.innerHTML;
         
         if(!calculated){
-        
-            if(btnVal=='AC'){
-                input.innerHTML = '';
-                decimalAddedd = false;
-            }
-            else if(btnVal=='='){
-                var equation = inputVal;
-                var lastChar = equation[equation.length - 1];
+            switch (btnVal) {
+                case "AC":
+                    input.innerHTML = '';
+                    decimalAddedd = false;
+                    break;
+                case "=":
+                    var equation = inputVal;
+                    var lastChar = equation[equation.length - 1];
                 
-                if(operators.indexOf(lastChar) > -1 || lastChar == '.'){
-                    equation = equation.replace(/.$/,'');
-                }
+                    if(operators.indexOf(lastChar) > -1 || lastChar == '.'){
+                        equation = equation.replace(/.$/,'');
+                    }
                 
-                if(equation)
-                    input.innerHTML = eval(equation);
+                    if(equation)
+                        input.innerHTML = eval(equation);
                     
-                decimalAddedd = false;
-                calculated = true;
-            }
-            else if(operators.indexOf(btnVal) > -1){
-                var lastChar = inputVal[inputVal.length - 1];
+                    decimalAddedd = false;
+                    calculated = true;
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    var lastChar = inputVal[inputVal.length - 1];
                 
-                if(inputVal != '' && operators.indexOf(lastChar) == -1)
-                    input.innerHTML += btnVal;
-                
-                else if(inputVal == '' && btnVal == '-')
-                    input.innerHTML += btnVal;
+                    if(inputVal != '' && operators.indexOf(lastChar) == -1)
+                        input.innerHTML += btnVal;
                     
-                if(operators.indexOf(lastChar) > -1 && inputVal.length > 1){
-                    input.innerHTML = inputVal.replace(/.$/,btnVal);
-                }
-                decimalAddedd = false;
-            }
-            else if(btnVal == '.'){
-                if(!decimalAddedd){
+                    else if(inputVal == '' && btnVal == '-')
+                        input.innerHTML += btnVal;
+                        
+                    if(operators.indexOf(lastChar) > -1 && inputVal.length > 1){
+                        input.innerHTML = inputVal.replace(/.$/,btnVal);
+                    }
+                    decimalAddedd = false;
+                    break;
+                case ".":
+                    if(!decimalAddedd){
+                        input.innerHTML += btnVal;
+                        decimalAddedd = true;
+                    }
+                    break;
+                default:
                     input.innerHTML += btnVal;
-                    decimalAddedd = true;
-                }
-            }
-            else{
-                input.innerHTML += btnVal;
             }
         }
         else{
-            if(btnVal=='AC'){
-                input.innerHTML = '';
-                decimalAddedd = false;
-            }
-            else if(btnVal=='='){
-                var equation = inputVal;
-                var lastChar = equation[equation.length - 1];
+            switch (btnVal) {
+                case "AC":
+                    input.innerHTML = '';
+                    decimalAddedd = false;
+                    break;
+                case "=":
+                    var equation = inputVal;
+                    var lastChar = equation[equation.length - 1];
                 
-                if(operators.indexOf(lastChar) > -1 || lastChar == '.'){
-                    equation = equation.replace(/.$/,'');
-                }
+                    if(operators.indexOf(lastChar) > -1 || lastChar == '.'){
+                        equation = equation.replace(/.$/,'');
+                    }
                 
-                if(equation)
-                    input.innerHTML = eval(equation);
+                    if(equation)
+                        input.innerHTML = eval(equation);
                     
-                decimalAddedd = false;
-            }
-            else if(operators.indexOf(btnVal) > -1){
-                var lastChar = inputVal[inputVal.length - 1];
+                    decimalAddedd = false;
+                    calculated = true;
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    var lastChar = inputVal[inputVal.length - 1];
                 
-                if(inputVal != '' && operators.indexOf(lastChar) == -1)
-                    input.innerHTML += btnVal;
-                
-                else if(inputVal == '' && btnVal == '-')
-                    input.innerHTML += btnVal;
+                    if(inputVal != '' && operators.indexOf(lastChar) == -1)
+                        input.innerHTML += btnVal;
                     
-                if(operators.indexOf(lastChar) > -1 && inputVal.length > 1){
-                    input.innerHTML = inputVal.replace(/.$/,btnVal);
-                }
-                decimalAddedd = false;
-            }
-            else if(btnVal == '.'){
-                if(!decimalAddedd){
-                    input.innerHTML += btnVal;
-                    decimalAddedd = true;
-                }
-            }   
-            else{
-                input.innerHTML = btnVal;
+                    else if(inputVal == '' && btnVal == '-')
+                        input.innerHTML += btnVal;
+                        
+                    if(operators.indexOf(lastChar) > -1 && inputVal.length > 1){
+                        input.innerHTML = inputVal.replace(/.$/,btnVal);
+                    }
+                    decimalAddedd = false;
+                    calculated = true;
+                    break;
+                case ".":
+                    if(!decimalAddedd){
+                        input.innerHTML += btnVal;
+                        decimalAddedd = true;
+                    }
+                    break;
+                default:
+                    input.innerHTML = btnVal;
             }
             calculated = false;
         }
